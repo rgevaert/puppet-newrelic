349fb1571e8216f81588b4de8d3f8b02cf1158af
Rudy Gevaert <rudy@webworm.org>
2015-06-17 13:25:39 +0200

    set labels and use template to manage nrsysmond.cfg

--- manifests/sysmond.pp
+++ manifests/sysmond.pp
@@ -1,40 +1,32 @@
-class newrelic::sysmond ($license)
+class newrelic::sysmond ($license, $labels='')
 {
-    include newrelic::repo
+  include newrelic::repo
 
-    $newrelic_license = $license
+  $newrelic_license = $license
+  if $newrelic_license == undef{ fail('$newrelic_license not defined') }
 
-    package { "newrelic-sysmond":
-        ensure  => latest,
-        notify  => Service['newrelic-sysmond'],
-        require => Class["newrelic::repo"];
-    }
-  
-    if $newrelic_license == undef{ fail('$newrelic_license not defined') }
+  package { 'newrelic-sysmond':
+      ensure  => latest,
+      notify  => Service['newrelic-sysmond'],
+      require => Class['newrelic::repo'];
+  }
 
-    Exec['newrelic-set-license', 'newrelic-set-ssl'] {
-      path +> ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']
-    }
+  file{
+    '/etc/newrelic/nrsysmond.cfg':
+      ensure  => present, 
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['newrelic-sysmond'],
+      notify  => Service['newrelic-daemon'],
+      content => template('newrelic/nrsysmond.cfg.erb');
+  }
 
-    exec { "newrelic-set-license":
-        unless  => "egrep -q '^license_key=${newrelic_license}$' /etc/newrelic/nrsysmond.cfg",
-        command => "nrsysmond-config --set license_key=${newrelic_license}",
-        require => Package['newrelic-sysmond'],
-        notify => Service['newrelic-sysmond'];
-    }
-
-    exec { "newrelic-set-ssl":
-        unless  => "egrep -q ^ssl=true$ /etc/newrelic/nrsysmond.cfg",
-        command => "nrsysmond-config --set ssl=true",
-        require => Package['newrelic-sysmond'],
-        notify => Service['newrelic-sysmond'];
-    }
-
-    service { "newrelic-sysmond":
-        enable  => true,
-        ensure  => running,
-        hasstatus => true,
-        hasrestart => true,
-        require => Package['newrelic-sysmond'];
-    }
+  service { 'newrelic-sysmond':
+    enable     => true,
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Package['newrelic-sysmond'];
+  }
 }
