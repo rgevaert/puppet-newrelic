2b04ace837e07ad86806b6a392a471e22878b5fe
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2015-09-21 10:39:07 +0200

    support removal of newrelic from system

--- manifests/php5.pp
+++ manifests/php5.pp
@@ -1,22 +1,43 @@
-class newrelic::php5($license, $appname = "PHP Application", $config_content = 'newrelic/newrelic.cfg.erb', $phpini_content = 'newrelic/php.ini.erb' )
+# Set up newrelic php5 monitoring
+class newrelic::php5( $license,
+                      $ensure         = 'present',
+                      $appname        = 'PHP Application',
+                      $config_content = 'newrelic/newrelic.cfg.erb',
+                      $phpini_content = 'newrelic/php.ini.erb' )
 {
   include newrelic::repo
 
   $newrelic_license = $license
 
+  case $ensure {
+    'present': {
+      $package_ensure = 'latest'
+      $service_ensure = 'running'
+      $link_ensure    = 'link'
+      $file_ensure    = 'present'
+    }
+    'absent': {
+      $package_ensure = 'absent'
+      $service_ensure = 'stopped'
+      $link_ensure    = 'absent'
+      $file_ensure    = 'absent'
+    }
+    default: {fail('ensure must be present or absent')}
+  }
+
   package {
     'newrelic-php5':
-      ensure  => latest,
+      ensure  => $package_ensure,
       notify  => Service['newrelic-daemon'],
       require => Class['newrelic::repo'];
   }
 
-  if $newrelic_license == undef{ fail("$newrelic_license not defined") }
+  if $newrelic_license == undef{ fail("${newrelic_license} not defined") }
 
   service {
     'newrelic-daemon':
+      ensure     => $service_ensure,
       enable     => true,
-      ensure     => running,
       provider   => 'init',
       hasstatus  => true,
       hasrestart => true,
@@ -28,32 +49,28 @@ class newrelic::php5($license, $appname = "PHP Application", $config_content = '
   if $::php_version == '' or versioncmp($::php_version, '5.5') >= 0 {
     $config_root_ini = "${config_root}/mods-available"
 
-    $link_ensure = $ensure ? {
-      'absent' => 'absent',
-      default  => 'link',
-    }
-
     file {
       "${config_root}/apache2/conf.d/20-newrelic.ini":
         ensure  => $link_ensure,
-        target  => "../../mods-available/newrelic.ini",
-        notify  => Service["httpd"];
+        target  => '../../mods-available/newrelic.ini',
+        notify  => Service['httpd'];
     }
 
     file {
       "${config_root}/cli/conf.d/20-newrelic.ini":
         ensure  => $link_ensure,
-        target  => "../../mods-available/newrelic.ini",
-        notify  => Service["httpd"];
+        target  => '../../mods-available/newrelic.ini',
+        notify  => Service['httpd'];
     }
 
     file {
       ["${config_root}/apache2/conf.d/newrelic.ini", "${config_root}/cli/conf.d/newrelic.ini"]:
+        ensure  => $file_ensure,
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
         content => template($phpini_content),
-        notify  => Service["httpd"],
+        notify  => Service['httpd'],
         require => Package['newrelic-php5'];
     }
 
@@ -61,18 +78,19 @@ class newrelic::php5($license, $appname = "PHP Application", $config_content = '
     $config_root_ini = "${config_root}/conf.d"
     file {
       "${config_root_ini}/newrelic.ini":
+        ensure  => $file_ensure,
         owner   => 'root',
         group   => 'root',
         mode    => '0640',
         content => template($phpini_content),
-        notify  => Service["httpd"],
+        notify  => Service['httpd'],
         require => Package['newrelic-php5'];
     }
   }
 
   file {
     '/etc/newrelic/newrelic.cfg':
-      ensure  => present, 
+      ensure  => $file_ensure,
       owner   => 'newrelic',
       group   => 'newrelic',
       mode    => '0640',
--- manifests/sysmond.pp
+++ manifests/sysmond.pp
@@ -1,19 +1,36 @@
-class newrelic::sysmond ($license, $labels='')
+# Newrelic system monitoring
+class newrelic::sysmond ($license, $ensure = 'present', $labels='')
 {
   include newrelic::repo
+
+  case $ensure {
+    'present': {
+      $package_ensure = 'latest'
+      $service_ensure = 'running'
+      $link_ensure    = 'link'
+      $file_ensure    = 'present'
+    }
+    'absent': {
+      $package_ensure = 'absent'
+      $service_ensure = 'stopped'
+      $link_ensure    = 'absent'
+      $file_ensure    = 'absent'
+    }
+    default: {fail('ensure must be present or absent')}
+  }
 
   $newrelic_license = $license
   if $newrelic_license == undef{ fail('$newrelic_license not defined') }
 
   package { 'newrelic-sysmond':
-      ensure  => latest,
+      ensure  => $package_ensure,
       notify  => Service['newrelic-sysmond'],
       require => Class['newrelic::repo'];
   }
 
   file{
     '/etc/newrelic/nrsysmond.cfg':
-      ensure  => present, 
+      ensure  => $file_ensure,
       owner   => 'newrelic',
       group   => 'newrelic',
       mode    => '0640',
@@ -23,8 +40,8 @@ class newrelic::sysmond ($license, $labels='')
   }
 
   service { 'newrelic-sysmond':
+    ensure     => $service_ensure,
     enable     => true,
-    ensure     => running,
     hasstatus  => true,
     hasrestart => true,
     require    => Package['newrelic-sysmond'];
